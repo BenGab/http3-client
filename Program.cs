e5dfb63019fecba783dd2ceeb434e8684132b7a6
bengab <benkogabor@msn.com>
2023-11-23 11:19:10 +0100

    basic http3 client

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+
+var handler = new HttpClientHandler();
+handler.ClientCertificateOptions = ClientCertificateOption.Manual;
+handler.ServerCertificateCustomValidationCallback = 
+    (httpRequestMessage, cert, cetChain, policyErrors) =>
+{
+    return true;
+};
+
+using var client = new HttpClient(handler) {
+    DefaultRequestVersion = HttpVersion.Version30,
+    DefaultVersionPolicy = HttpVersionPolicy.RequestVersionExact
+};
+
+
+
+Console.WriteLine("--- localhost:5001 ---");
+
+HttpResponseMessage resp = await client.GetAsync("https://localhost:5001/");
+string body = await resp.Content.ReadAsStringAsync();
+
+Console.WriteLine(
+    $"status: {resp.StatusCode}, version: {resp.Version}, " +
+    $"body: {body.Substring(0, Math.Min(100, body.Length))}");
